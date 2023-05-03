0dec9d333dedc191b2dd18ac046f346b997eaae8
toratapp <73744488+toratapp@users.noreply.github.com>
2023-05-03 13:31:41 +0200

    Worked on the home page and login page

---
+++ js/components/footer.js
@@ -0,0 +1,20 @@
+const footer = document.querySelector("footer");
+
+footer.innerHTML += `<div class="footer-part-one d-flex">
+                        <div class="footer__socials row">
+                          <h4>Socials</h4>
+                          <i class="fa-brands fa-square-facebook"></i>
+                          <i class="fa-brands fa-square-instagram"></i>
+                          <i class="fa-brands fa-tiktok"></i>
+                        </div>
+                        <div class="footer__short-menu row">
+                            <h4>Short menu</h4>
+                            <a href="#" class="mb-1 mt-2"><p>Auctions</p></a>
+                            <a href="#" class="mb-0"><p>Login</p></a>
+                            <a href="#" class="mb-0"><p>Terms</p></a>
+                        </div>
+                    </div>
+                    <div class="footer-part-two text-center mt-3">
+                      <p>&copy Copyright 2023 - Bid & buy co</p>
+                    </div>`
+                    
